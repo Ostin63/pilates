79340f71e0ee203eecb6819143f0f76c240595aa
DESKTOP-4LGQ79I\ostin <upssq@mail.ru>
2019-03-19 15:20:07 +0400

    валидация формы

--- js/style.js
+++ js/style.js
@@ -3,33 +3,72 @@ $(document).ready(function () {
     loop: true,
     items: 1,
     center: false,
-    responsive: { 
+    responsive: {
       0: {
         nav: false,
         margin: 50
-        
+
       },
       764: {
-        nav:true
+        nav: true
       },
       1366: {
-        nav:true
+        nav: true
       }
     }
   });
-  $('.intro__button').on('click', function(evt){
+  $('.intro__button').on('click', function (evt) {
     $('.intro-modal').removeClass('d-none').addClass('show')
   });
   $('.modal-form__close').on('click', function (evt) {
     $('.intro-modal').addClass('d-none').removeClass('show')
   });
-  $('.main-nav__toogles').on('click', function(evt) {
+  $('.main-nav__toogles').on('click', function (evt) {
     $('.header-modal__container').removeClass('d-none').addClass('show')
   });
-  $('.modal-close').on('click', function (evt){
+  $('.modal-close').on('click', function (evt) {
     $('.header-modal__container').addClass('d-none').removeClass('show')
   });
-  $('.modal-manu a').on('click', function(evt){
+  $('.modal-manu a').on('click', function (evt) {
     $('.header-modal__container').addClass('d-none').removeClass('show')
-  })
+  });
+
+  jQuery.validator.addMethod("checkMask", function (value, element) {
+    return /\+\d{1}\(\d{3}\)\d{3}-\d{4}/g.test(value);
+  });
+
+  $('.modal-form').validate({
+    rules: {
+      fname: {
+        required: true,
+        minlength: 2
+      },
+      fmail: {
+        required: true,
+        email: true
+      },
+      fphone: {
+        required: true,
+        checkMask: true
+      }
+    },
+    messages: {
+      fname: {
+        required: "Это поле обязательно",
+        minlength: "Введите не менее 2-х символов в поле 'Имя'"
+      },
+      fmail: {
+        required: "Это поле обязательно",
+        email: "Необходим формат адреса email"
+      },
+      fphone: {
+        required: "Это поле обязательно",
+        checkMask: "Введите полный номер телефона"
+      },
+      fcheckbox : {
+        required: "Необходимо Ваше согласие"
+      }
+    }
+  });
+  $('#phone').mask("+7(999)999-9999", { autoclear: false });
 });
